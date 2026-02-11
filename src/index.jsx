3f7ec3d0a00d8dc01e26efb891966ddb3e2fd250
IHAR STALIAROU <ihar.staliarou.dev@gmail.com>
2026-02-11 18:18:54 +0300

    config: restore project

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import App from './components/app/app';
-import './index.scss';
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
